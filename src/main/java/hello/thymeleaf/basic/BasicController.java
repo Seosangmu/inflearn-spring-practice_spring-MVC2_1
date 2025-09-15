3c43283d9c94587676b84bdcb43b5689f44872ea
ssm <tjtkdan1072@gmail.com>
2025-09-16 02:20:54 +0900

    타임리프 기본 객체들

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,11 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +49,15 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @RequestMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
     @Data
     static class User {
         private String username;
@@ -52,6 +66,13 @@ public class BasicController {
         public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    @Component("helloBean")
+    class hello {
+        public String hello(String data) {
+            return "Hello" + data;
         }
     }
 }
